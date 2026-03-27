9d0062fe1ef88ecf924b82b2883cd2ef29a5061b
stoval <robbertml@gmail.com>
2026-03-27 20:48:43 +1100

    增强功能：人脸对齐剪影、PWA 离线支持、中大图降采样性能优化

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'visaphoto-v1';
+const ASSETS = [
+  'passport-photo-maker.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700&family=DM+Mono:wght@400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
